17a3a0ca094da84981177e09829c8a2ad498545f
Seraban09 <101631947+Seraban09@users.noreply.github.com>
2022-07-11 14:26:10 +0200

    Modificato Hello, per l'esercizio 6

--- src/Hello.js
+++ src/Hello.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react'
 export class Hello extends Component {
   render() {
     return (
-      <div><h1>"Hello, World!"</h1></div>
+      <div><h1>"Hello, World!"</h1>
+      <p>"What a beautiful day!"</p>
+      </div>
+
     )
   }
 }
