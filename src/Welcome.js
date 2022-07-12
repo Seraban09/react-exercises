f75fd34f139c6cdbb39aa90af2417ed2548fa2a6
Seraban09 <101631947+Seraban09@users.noreply.github.com>
2022-07-12 15:27:14 +0200

    conditional-rendering03 done

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import { Age } from './Age'
 
-
+let age= 66;
 export class Welcome extends Component {
   render() {
     return (
       <div>Welcome, {this.props.name}!
-      {!this.props.eta && <Age  /> }
+      {age>=18 && age<65 ? <Age eta={age} /> : <strong>dato non valido</strong> }
       </div>
     )
   }
