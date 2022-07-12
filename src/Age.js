8f7ad4f65d669b417fcc8e6b74e79d2ab86fe058
Seraban09 <101631947+Seraban09@users.noreply.github.com>
2022-07-12 10:36:45 +0200

    conditional rendering 01 done

--- src/Age.js
+++ src/Age.js
@@ -3,12 +3,12 @@ import React, { Component } from 'react'
 export class Age extends Component {
   render() {
     return (
-      <div>Your age is {this.props.age}!</div>
+      <div>Your age is {this.props.eta}!</div>
     )
   }
 }
 
 
 Age.defaultProps = {
-    age: <strong>28</strong>
+    eta: <strong>29</strong>
 }
--- src/Welcome.js
+++ src/Welcome.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import { Age } from './Age'
 
+let age = 25;
 export class Welcome extends Component {
   render() {
     return (
       <div>Welcome, {this.props.name}!
-      <Age />
+      {age >= 18 ? <Age eta = {age}/> :< strong>Attento sei un Minorenne</strong>}
       </div>
     )
   }
@@ -13,5 +14,4 @@ export class Welcome extends Component {
 
 Welcome.defaultProps = {
     name: <strong>Pippo</strong>,
-    
   }
