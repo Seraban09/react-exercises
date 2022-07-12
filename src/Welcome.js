28601f129e3e36d9627db498e4bd0f27a4a67221
Seraban09 <101631947+Seraban09@users.noreply.github.com>
2022-07-12 15:08:53 +0200

    preparazione rendering 02

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import { Age } from './Age'
 
-let age = 25;
+
 export class Welcome extends Component {
   render() {
     return (
       <div>Welcome, {this.props.name}!
-      {age >= 18 ? <Age eta = {age}/> :< strong>Attento sei un Minorenne</strong>}
+      {age  ? <Age eta = {age}/> :< strong>Attento sei un Minorenne</strong>}
       </div>
     )
   }
